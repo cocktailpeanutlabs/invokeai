23b59550786cba8f0e2b8c3facf517ba525d01de
cocktailpeanut <cocktailpeanuts@proton.me>
2024-01-03 16:15:25 -0500

    init

---
+++ pinokio.js
@@ -0,0 +1,45 @@
+const path = require("path")
+module.exports = {
+  title: "InvokeAI",
+  description: "Generative AI for Professional Creatives",
+  icon: "icon.png",
+  menu: async (kernel) => {
+    let installing = kernel.running(__dirname, "install.json")
+    let installed = await kernel.exists(__dirname, "app", "env")
+    if (installing) {
+      return [{ icon: "fa-solid fa-plug", text: "Installing...", href: "install.json" }]
+    } else if (installed) {
+      let running = kernel.running(__dirname, "start.json")
+      if (running) {
+        let memory = kernel.memory.local[path.resolve(__dirname, "start.json")]
+        if (memory && memory.url) {
+          return [
+            { icon: "fa-solid fa-rocket", text: "Web UI", href: memory.url },
+            { icon: "fa-solid fa-terminal", text: "Terminal", href: "start.json" },
+            { icon: "fa-solid fa-rotate", text: "Update", href: "update.json" },
+          ]
+        } else {
+          return [
+            { icon: "fa-solid fa-terminal", text: "Terminal", href: "start.json" },
+            { icon: "fa-solid fa-rotate", text: "Update", href: "update.json" },
+          ]
+        }
+      } else {
+        return [{
+          icon: "fa-solid fa-power-off",
+          text: "Start",
+          href: "start.json",
+        }, {
+          icon: "fa-solid fa-rotate", text: "Update", href: "update.json"
+        }, {
+          icon: "fa-solid fa-plug", text: "Fresh reinstall", href: "install.json"
+        }]
+      }
+    } else {
+      return [
+        { icon: "fa-solid fa-plug", text: "Install", href: "install.json" },
+        { icon: "fa-solid fa-rotate", text: "Update", href: "update.json" }
+      ]
+    }
+  }
+}
